8dad2eeacc994c18def7546023a9b99340e91670
Stephen Celis <stephen@stephencelis.com>
2013-12-23 10:56:41 -0800

    Initial Commit

---
+++ syn/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  syn
+//
+//  Created by Stephen Celis on 12/23/13.
+//  Copyright (c) 2013 stephencelis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
