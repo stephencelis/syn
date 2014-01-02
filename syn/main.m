08511f63e067e8dedffaddec524d911d966faf26
Stephen Celis <stephen@stephencelis.com>
2014-01-01 18:14:06 -0800

    Bump version to 0.2.1
    What's changed?
    
    - More accurate error messaging (thanks to BRLOptionParser 0.2.0).
    
    Signed-off-by: Stephen Celis <stephen@stephencelis.com>

--- syn/main.m
+++ syn/main.m
@@ -34,7 +34,7 @@ int main(int argc, const char * argv[])
         // constants
 
         NSString *processName = [[NSProcessInfo processInfo] processName];
-        static NSString *const VERSION = @"0.2.0";
+        static NSString *const VERSION = @"0.2.1";
 
         // configuration
 
